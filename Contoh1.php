e55493ccd4b512497b790cbcef0ac4c4ea3dfd69
axpikaa <arvika498@gmail.com>
2022-03-21 10:57:24 +0700

    Menambahkan file index

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Arvika Ayuni Rahman Saya tinggal di daerah Sawangan Saya tidak menyukai olahraga mwehehe";
+}
+}
