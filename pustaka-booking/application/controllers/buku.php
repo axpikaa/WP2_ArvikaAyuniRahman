1dd43440a4a773fbd7ca0473e9e9dc86b872e724
axpikaa <arvika498@gmail.com>
2022-06-14 11:00:03 +0700

    Menambahkan File

--- pustaka-booking/application/controllers/buku.php
+++ pustaka-booking/application/controllers/buku.php
@@ -106,37 +106,42 @@ class Buku extends CI_Controller
  			redirect('buku/kategori');
  		}
  	}
- 	public function hapusKategori()
- 	{
- 		$where = ['id' => $this->uri->segment(3)];
- 		$this->ModelBuku->hapusKategori($where);
- 		redirect('buku/kategori');
- 	}
- 	public function ubahkategori()
- 	{
- 		$data['judul'] = 'Ubah Data Kategori';
- 		$data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
- 		$data['kategori'] = $this->ModelBuku->kategoriWhere(['id' => $this->uri->segment(3)])->result_array();
- 		$this->form_validation->set_rules('kategori', 'Nama Kategori', 'required|min_length[3]', [
-			'required' => 'Nama Kategori harus diisi',
-			'min_length' => 'Nama Kategori terlalu pendek'
- 		]);
- 		if ($this->form_validation->run() == false) {
- 			$this->load->view('templates/header', $data);
- 			$this->load->view('templates/sidebar', $data);
- 			$this->load->view('templates/topbar', $data);
- 			$this->load->view('buku/kategori', $data);
- 			$this->load->view('templates/footer');
- 		} else {
- 			$data = [
- 				'kategori' => $this->input->post('kategori', true)
- 			];
- 			$this->ModelBuku->updateKategori(['id'=> $this->input->post('id')], $data);
- 			redirect('buku/kategori');
- 			$this->session->set_flashdata('pesan', '<div class="alert-danger" role="alert">kategori Berhasil diupdate </div>');
- 			redirect('buku/kategori'); 		
- 	}
- }
+     public function ubahkategori()
+     {
+         $data['judul'] = 'Ubah Kategori Buku';
+         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+ 
+         $where = ['id' =>  $this->uri->segment(3)];
+         $data['kategori'] = $this->ModelBuku->kategoriWhere($where)->row_array();
+         
+         
+         $this->form_validation->set_rules(
+             'kategori', 
+             'Kategori',
+             'required', [
+                 'required' => 'Judul Buku harus diisi'
+         ]);
+ 
+         if ($this->form_validation->run() == false) 
+         {
+             $this->load->view('templates/header', $data);
+             $this->load->view('templates/sidebar', $data);
+             $this->load->view('templates/topbar', $data);
+             $this->load->view('buku/ubah_kategori', $data);
+             $this->load->view('templates/footer');
+         } else {
+             $data = ['kategori' => $this->input->post('kategori')];
+             $this->ModelBuku->updateKategori(['id' => $this->input->post('id')], $data);
+             redirect('buku/kategori');
+         }
+     }
+ 
+     public function hapusKategori()
+     {
+         $where = ['id' => $this->uri->segment(3)];
+         $this->ModelBuku->hapusKategori($where);
+         redirect('buku/kategori');
+     } 
  	public function ubahBuku(){
         $data['judul'] = 'Ubah Data Buku';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
